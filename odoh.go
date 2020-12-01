a9ea7b040a654d6a7f690f738a6f182639847080
Christopher Wood <caw@heapingbits.net>
2020-12-01 06:50:11 -0800

    Fix license on source files.

--- odoh.go
+++ odoh.go
@@ -1,6 +1,6 @@
 // The MIT License
 //
-// Copyright (c) 2019 Apple, Inc.
+// Copyright (c) 2019-2020, Cloudflare, Inc. and Apple, Inc. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
